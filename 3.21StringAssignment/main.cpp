d6d640374e8c5c1ae205ee960cbbf30256341b62
Heritage Samuel F <30316855+Herrytheeagle@users.noreply.github.com>
2017-10-03 13:36:59 +0100

    Add files via upload

---
+++ 3.21StringAssignment/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+  char word[100];
+  char statement[100]=("Olu is a boy,he lives in calabar.he is ten years old,but he is a stuborn boy.");
+  cout<<"KINDLY TYPE IN ANY WORD"<<endl;
+  cin>>word;
+  //string word1 = word;
+ /* string st =*/if (strstr(statement,word));
+//cout << st.substr(0,word1.size());
+
+cout<<"true";
+return 0;
+}
---
+++ 4.7 Loop assignment/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numberOfInput =0;
+    int tution =10000;
+     int initialTution = 10000;
+    int increment=0;
+
+
+    for(int num=0; num<v10; num++){
+    cout <<tution<<endl;
+    tution = tution + ((5*tution)/100);
+
+    }
+    cout<<"tution is: "<<tution;
+    return 0;
+}
